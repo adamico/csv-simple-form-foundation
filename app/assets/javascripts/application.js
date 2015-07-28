8d145ecee5cfa670b41c406c8fb70ce9f87957ff
Andrea D'Amico <andrea.damico@gmail.com>
2015-07-28 10:43:36 +0200

    init client side validation and user scaffold

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require rails.validations
+//= require rails.validations.simple_form
+//= require foundation
 //= require turbolinks
 //= require_tree .
